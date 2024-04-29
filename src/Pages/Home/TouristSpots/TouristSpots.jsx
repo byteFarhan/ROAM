52a95e9b2fa004e049f01153ae5d7790f925a6a6
farhan-mazumder <farhanmazumder75@gmail.com>
2024-04-29 16:22:50 +0600

    load tourist spots from the database using local server.

--- src/Pages/Home/TouristSpots/TouristSpots.jsx
+++ src/Pages/Home/TouristSpots/TouristSpots.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import SectionIntro from "../../Shared/Navbar/SectionIntro/SectionIntro";
+import { useEffect } from "react";
 
 const TouristSpots = () => {
+  const [touristSpots, setTouristSpots] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/tourist_spots")
+      .then((res) => res.json())
+      .then((data) => setTouristSpots(data));
+  }, []);
+  console.log(touristSpots);
   return (
     <section id="tourist-spot" className="my-20 lg:my-24">
       <SectionIntro
@@ -8,6 +17,7 @@ const TouristSpots = () => {
         title={`Our Most Popular Adventures`}
         description={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi posuere tempor mauris, nec imperdiet mi rutrum eget. Donec quis ligula in tellus dictum consequat.`}
       />
+      <div></div>
     </section>
   );
 };
