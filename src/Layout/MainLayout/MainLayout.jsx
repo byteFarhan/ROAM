1f54a8f97cfcfbdbec8054197239d6cb8a630f73
farhan-mazumder <farhanmazumder75@gmail.com>
2024-04-29 23:29:05 +0600

    Footer has been added.

--- src/Layout/MainLayout/MainLayout.jsx
+++ src/Layout/MainLayout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../Pages/Shared/Navbar/Navbar";
+import Footer from "../../Pages/Shared/Footer/Footer";
 
 const MainLayout = () => {
   // const {}
@@ -7,6 +8,7 @@ const MainLayout = () => {
     <section>
       <Navbar />
       <Outlet />
+      <Footer />
     </section>
   );
 };
