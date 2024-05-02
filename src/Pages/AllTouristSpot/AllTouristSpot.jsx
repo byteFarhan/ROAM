0baf82d92b64499b7235011b3b656600aa65ae1e
farhan-mazumder <farhanmazumder75@gmail.com>
2024-05-03 00:35:06 +0600

    Sort section disign part has been completed.

--- src/Pages/AllTouristSpot/AllTouristSpot.jsx
+++ src/Pages/AllTouristSpot/AllTouristSpot.jsx
@@ -2,27 +2,78 @@ import { useLoaderData } from "react-router-dom";
 import TouristSpotB from "./TouristSpotB/TouristSpotB";
 import Banner from "../Shared/Banner/Banner";
 import bannerImg from "../../assets/banner-img.jpg";
+import { useState } from "react";
+import { MdFileUpload, MdOutlineSortByAlpha } from "react-icons/md";
+import { ImDownload2, ImUpload, ImUpload2 } from "react-icons/im";
+import { IoIosStarHalf } from "react-icons/io";
+import { FaSortAlphaDown } from "react-icons/fa";
 
 const AllTouristSpot = () => {
   const touristSpots = useLoaderData();
   console.log(touristSpots);
+  console.log(touristSpots.slice());
+  const [sortedSpots, setSortedSpots] = useState([]);
+  const [activeItem, setActiveItem] = useState(null);
+  const handleItemClick = (item) => {
+    setActiveItem(item);
+  };
 
   return (
     <section className="">
       <Banner bannerTitle={`Tour Search`} bannerImg={bannerImg} />
-      <div>
-        <h3 className="my-12 text-3xl font-bold text-center capitalize text-primary">
-          Sort functonallity will implement here
-        </h3>
+      {/* Sort tourist spots Section */}
+      <div className="bg-[#f8f8f8]">
+        <ul
+          className="flex flex-col md:flex-row items-center *:text-natural *:lg:text-lg *:font-medium *:lg:font-semibold
+         justify-between *:py-6 mx-auto *:text-center max-w-7xl *:border-r *:border-base-300 *:flex *:justify-center *:items-center *:gap-2 *:w-full hover:*:text-primary *:cursor-pointer *:transition-colors *:delay-75 *:duration-75 *:ease-in-out"
+        >
+          <li
+            onClick={() => handleItemClick("name")}
+            className={`${activeItem === "name" ? "bg-white border-none" : ""}`}
+          >
+            <FaSortAlphaDown />
+            Name
+          </li>
+          <li
+            onClick={() => handleItemClick("lowToHigh")}
+            className={`${
+              activeItem === "lowToHigh" ? "bg-white border-none" : ""
+            }`}
+          >
+            {/* <ImUpload /> */}
+            <ImUpload2 />
+            Price low to high
+          </li>
+          <li
+            onClick={() => handleItemClick("highToLow")}
+            className={`${
+              activeItem === "highToLow" ? "bg-white border-none" : ""
+            }`}
+          >
+            <ImDownload2 />
+            Price high to low
+          </li>
+          <li
+            onClick={() => handleItemClick("rating")}
+            className={`${
+              activeItem === "rating" ? "bg-white border-none" : ""
+            }`}
+          >
+            <IoIosStarHalf />
+            Rating
+          </li>
+        </ul>
       </div>
-      <section className="grid gap-6 max-w-[1280px] mx-auto px-5 lg:px-0 my-12 lg:grid-cols-4">
+      <section className="grid gap-6 max-w-[1280px] mx-auto px-5 lg:px-0 my-24 lg:grid-cols-4">
         <div className="flex flex-col gap-10 lg:col-span-3">
           {touristSpots &&
             touristSpots?.map((spot) => (
               <TouristSpotB key={spot._id} theSpot={spot} />
             ))}
         </div>
-        <div className="border-2 lg:col-span-1 border-title"></div>
+        <div className="lg:col-span-1">
+          <div className="lg:h-[80vh]  border-title border-2 "></div>
+        </div>
       </section>
     </section>
   );
